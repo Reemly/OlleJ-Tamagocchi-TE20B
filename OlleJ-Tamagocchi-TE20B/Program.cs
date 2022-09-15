8b60d99dd9cb207ebab7f5c5fe58eb0b63a15db4
Olle Järvheden <jarvheden.olle@elev.ga.ntig.se>
2022-09-15 11:10:03 +0200

    rrqre

--- OlleJ-Tamagocchi-TE20B/Program.cs
+++ OlleJ-Tamagocchi-TE20B/Program.cs
@@ -5,4 +5,14 @@
 Tamagotchi tama = new Tamagotchi();
 
 
+while (tama.GetAlive() == true ) {
+tama.printstats();
+Console.WriteLine("what u wanna do with ur little guy");
+Console.WriteLine($"");
+Console.WriteLine($"");
+Console.WriteLine($"");
+Console.WriteLine($"");
+}
 
+
+//fokusera på program.cs, tror allt e klart i tamagotchi.cs
--- OlleJ-Tamagocchi-TE20B/Tamagotchi.cs
+++ OlleJ-Tamagocchi-TE20B/Tamagotchi.cs
@@ -1,31 +1,74 @@
 using System;
-
+using System.Collections.Generic;
 public class Tamagotchi
 {
-    public bool isALive;
-    public int boredom;
-    public int hunger;
-    
+    private bool isALive;
+    private int boredom;
+    private int hunger;
+    private Random generator;
+    private List<string> words = new List<string>() { "no" };
+    public string name;
 
-    
-    
-    
-    public void Tick() {
-    int TickBored = 0;
-    int TickHunger = 0;
 
-    
+    public Tamagotchi()
+    {
+        generator = new Random;
+        isALive = true;
 
     }
-    
 
 
+    public void Tick()
+    {
+        hunger++;
+        boredom++;
 
+        if (hunger > 10 || boredom > 10)
+        {
+            isALive = false;
+        }
+
+    }
 
 
 
     public void Feed()
     {
-    
+        Console.WriteLine("m");
+        hunger -= 2;
+        if (hunger < 0)
+        {
+            hunger = 0;
+        }
     }
+
+
+    public void heyo()
+    {
+    int numoword = generator.Next(words.Count);
+    Console.WriteLine($"[{name}] says: {words[numoword]}");
+
+    }
+
+    public void ReduceBoredompls() 
+    {
+        Console.WriteLine($" [{name}] is less borred ヾ(＠^∇^＠)ノ");
+        boredom -= 2;
+        if (boredom < 0) {
+            boredom = 0;
+        }
+    }
+
+    public bool GetAlive() {
+        return isALive = true;
+    }
+
+
+
+
+
+    public void printstats() {
+        Console.WriteLine($"Name: {name} || Hunger: {hunger} || Boredom: {boredom} || Dictionary: {words.Count} words");
+    }
+
 }
